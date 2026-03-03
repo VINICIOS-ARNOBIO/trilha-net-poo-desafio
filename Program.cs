df95348ffa866852e0de4189f3fdc3dc516cfeba
VINICIOS-ARNOBIO <vinicios.arnobio2019@gmail.com>
2026-03-03 06:41:39 -0300

    Desafio concluido por Vinicios Arnóbio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("11999999999", "Tijolão", "123456789", 64);
+        Iphone iphone = new Iphone("11888888888", "iPhone 15", "987654321", 128);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
